e846e6bc22b4062856b75cd1dd6a912cbca495d3
agaha <aga.hagneia@gmail.com>
2022-06-11 20:11:05 +0200

    fight bird beta
    without health

--- Assets/Scripts/BirdBossController.cs
+++ Assets/Scripts/BirdBossController.cs
@@ -7,15 +7,19 @@ public class BirdBossController : MonoBehaviour
     private GameObject _target;
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _collider;
     private GameObject _orc;
     private GameObject _human;
     public float distance;
+    public LayerMask groundMask;
+    public float groundOverlapHeight;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
         _target = null;
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _orc = GameObject.Find("Orc");
         _human = GameObject.Find("Human");
@@ -33,21 +37,37 @@ public class BirdBossController : MonoBehaviour
                 _animator.SetInteger("Action", 1);
             }
         }
-        else if (_animator.GetInteger("Action") != 0)
+        else
         {
-            _animator.SetInteger("Action", 0);
+            if (!IsGrounded())
+            {
+                if (_animator.GetInteger("Action") != 1)
+                {
+                    _animator.SetInteger("Action", 1);
+                }
+
+                transform.position = Vector2.MoveTowards(transform.position,
+                    new Vector2(transform.position.x, transform.position.y - 1f),
+                    moveSpeed * Time.deltaTime);
+            }
+            else if (_animator.GetInteger("Action") != 0)
+            {
+                _animator.SetInteger("Action", 0);
+            }
         }
 
         _spriteRenderer.flipX = _rigidbody2D.velocity.x <= 0;
-
-        if (Vector2.Distance(transform.position, _orc.transform.position) < distance)
-        {
-            _target = _orc;
-        }
-
         if (Vector2.Distance(transform.position, _human.transform.position) < distance)
         {
             _target = _human;
+        }
+        else if (Vector2.Distance(transform.position, _orc.transform.position) < distance)
+        {
+            _target = _orc;
+        }
+        else
+        {
+            _target = null;
         }
     }
 
@@ -65,5 +85,19 @@ public class BirdBossController : MonoBehaviour
                 other.gameObject.GetComponent<PlayerController>().Die(); // Kill
             }
         }
+        else 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        var bounds = _collider.bounds;
+        var boxCenter = new Vector2(bounds.center.x, bounds.center.y) +
+                        (Vector2.down * (bounds.extents.y + (groundOverlapHeight / 2f)));
+        var boxSize = new Vector2(bounds.size.x, groundOverlapHeight);
+        var groundBox = Physics2D.OverlapBox(boxCenter, boxSize, 0f, groundMask);
+        return groundBox != null;
     }
 }
