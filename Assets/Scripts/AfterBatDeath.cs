84bd1e872bc67b9e3fbf1b6014b61f2a29bcd2e5
agaha <aga.hagneia@gmail.com>
2022-06-11 09:40:50 +0200

    lvl 3 fixes

--- Assets/Scripts/AfterBatDeath.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AfterBatDeath : StateMachineBehaviour
-{
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        Destroy(animator.gameObject);
-    }
-}
