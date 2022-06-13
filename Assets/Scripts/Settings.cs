a093c4420a64693de9b7b3b15cc9534baab74bdc
agaha <aga.hagneia@gmail.com>
2022-06-13 23:37:55 +0200

    save file created

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,26 +1,44 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using static System.Int32;
 
-public static class Settings
+public class Settings
 {
     //Save 1
-    private static int _humanLives1;
-    private static int _orcLives1;
-    private static string _level1;
-    private static string _mode1;
+    private int _humanLives1;
+    private int _orcLives1;
+    private string _level1;
+    private string _mode1;
 
     //Save 2
-    private static int _humanLives2;
-    private static int _orcLives2;
-    private static string _level2;
-    private static string _mode2;
+    private int _humanLives2;
+    private int _orcLives2;
+    private string _level2;
+    private string _mode2;
 
     //Save 3
-    private static int _humanLives3;
-    private static int _orcLives3;
-    private static string _level3;
-    private static string _mode3;
+    private int _humanLives3;
+    private int _orcLives3;
+    private string _level3;
+    private string _mode3;
 
-    public static int GetHumanLives(int save)
+    public Settings()
+    {
+        string[] lines = File.ReadAllLines(@"Resources\Saves.txt");
+        _humanLives1 = Parse(lines[0]);
+        _humanLives2 = Parse(lines[1]);
+        _humanLives3 = Parse(lines[2]);
+        _orcLives1 = Parse(lines[3]);
+        _orcLives2 = Parse(lines[4]);
+        _orcLives3 = Parse(lines[5]);
+        _level1 = lines[6];
+        _level2 = lines[7];
+        _level3 = lines[8];
+        _mode1 = lines[9];
+        _mode2 = lines[10];
+        _mode3 = lines[11];
+    }
+
+    public int GetHumanLives(int save)
     {
         return save switch
         {
@@ -30,8 +48,8 @@ public static class Settings
             _ => -1
         };
     }
-    
-    public static int GetOrcLives(int save)
+
+    public int GetOrcLives(int save)
     {
         return save switch
         {
@@ -41,9 +59,9 @@ public static class Settings
             _ => -1
         };
     }
-    
-    public static void SetHumanLives(int save, int lives)
-    { 
+
+    public void SetHumanLives(int save, int lives)
+    {
         switch (save)
         {
             case 1:
@@ -55,11 +73,13 @@ public static class Settings
             case 3:
                 _humanLives3 = lives;
                 break;
-        };
+        }
+
+        SaveAll();
     }
-    
-    public static void SetOrcLives(int save, int lives)
-    { 
+
+    public void SetOrcLives(int save, int lives)
+    {
         switch (save)
         {
             case 1:
@@ -71,10 +91,12 @@ public static class Settings
             case 3:
                 _orcLives3 = lives;
                 break;
-        };
+        }
+
+        SaveAll();
     }
 
-    public static string GetLevel(int save)
+    public string GetLevel(int save)
     {
         return save switch
         {
@@ -84,9 +106,9 @@ public static class Settings
             _ => "empty"
         };
     }
-    
-    public static void SetLevel(int save, string level)
-    { 
+
+    public void SetLevel(int save, string level)
+    {
         switch (save)
         {
             case 1:
@@ -98,10 +120,12 @@ public static class Settings
             case 3:
                 _level3 = level;
                 break;
-        };
+        }
+
+        SaveAll();
     }
-    
-    public static string GetMode(int save)
+
+    public string GetMode(int save)
     {
         return save switch
         {
@@ -111,9 +135,9 @@ public static class Settings
             _ => "empty"
         };
     }
-    
-    public static void SetMode(int save, string mode)
-    { 
+
+    public void SetMode(int save, string mode)
+    {
         switch (save)
         {
             case 1:
@@ -125,6 +149,21 @@ public static class Settings
             case 3:
                 _mode3 = mode;
                 break;
+        }
+
+        SaveAll();
+    }
+
+    private void SaveAll()
+    {
+        string[] lines =
+        {
+            _humanLives1.ToString(), _humanLives2.ToString(), _humanLives3.ToString(),
+            _orcLives1.ToString(), _orcLives2.ToString(), _orcLives3.ToString(),
+            _level1, _level2, _level3,
+            _mode1, _mode2, _mode3
         };
+
+        File.WriteAllLines(@"Resources\Saves.txt", lines);
     }
 }
