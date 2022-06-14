7ac13ad5df7ef52e31092b9de64c77ec15895c98
agaha <aga.hagneia@gmail.com>
2022-06-14 18:53:22 +0200

    Level 2 Completed by John

--- Assets/Scripts/EasyEnemyController.cs
+++ Assets/Scripts/EasyEnemyController.cs
@@ -25,6 +25,20 @@ public class EasyEnemyController : MonoBehaviour
                 col.GetComponent<PlayerController>().Die();
             }
         }
+
+        if (col.CompareTag("UserAttack"))
+        {
+            var playerAct = col.GetComponent<Animator>().GetInteger("Anim");
+            if (playerAct == 0 || playerAct == 1)
+            {
+                _animator.SetInteger("Action", 2);
+            }
+            else
+            {
+                _animator.SetInteger("Action", 1);
+                col.GetComponent<PlayerController>().Die();
+            }
+        }
     }
 
     private IEnumerator OnTriggerExit2D(Collider2D other)
